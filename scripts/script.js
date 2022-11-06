be1c9b92a51b3ae4d02c36bb77f103d4ae52be65
José Júnior <jredit7@gmail.com>
2022-11-06 14:03:25 -0300

    Corrige bugs de responsividade

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+// script alert
+    setTimeout(function() {
+    $('#alert').fadeOut('fast');
+    }, 9500);
+    
+// script alert mobile
+    setTimeout(function() {
+    $('#alertmobile').fadeOut('fast');
+    }, 22000);
